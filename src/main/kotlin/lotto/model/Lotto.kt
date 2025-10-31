dc2049bd60c56e20fa428219c86a2bde2cbdffb2
todays_sun_day <178suna@gmail.com>
2025-10-31 18:42:38 +0900

    refactor: 기존에 주어진 로또 클래스 수정
    데이터 클래스로 변경함.
    에러메시지를 상수로 관리함.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,8 +1,10 @@
 package lotto.model
 
-class Lotto(private val numbers: List<Int>) {
+import lotto.util.ErrorMessage
+
+data class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == 6) { ErrorMessage.INCORRECT_LOTTO_SIZE.fullMessage }
     }
 
     // TODO: 추가 기능 구현
--- src/main/kotlin/lotto/util/ErrorMessage.kt
+++ src/main/kotlin/lotto/util/ErrorMessage.kt
@@ -4,7 +4,8 @@ enum class ErrorMessage(private val message: String) {
     INPUT_IS_NULL_OR_EMPTY("입력값이 비어있습니다."),
     INPUT_NOT_NUMERIC("입력값이 숫자가 아닙니다."),
     INVALID_AMOUNT_UNIT("입력한 금액 단위가 유효하지 않습니다."),
-    INVALID_QUANTITY_NOT_MET("로또 구매 수량이 1개 미만입니다.");
+    INVALID_QUANTITY_NOT_MET("로또 구매 수량이 1개 미만입니다."),
+    INCORRECT_LOTTO_SIZE("로또 번호는 6개여야 합니다.");
 
     val fullMessage: String
         get() = "$PREFIX $message"
